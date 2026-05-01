7cefa9afcacb7af839da9733ae0289067bc5ac5d
Qaisalhaj <121435318+Qaisalhaj@users.noreply.github.com>
2026-05-01 13:29:56 +0300

    Add files via upload

---
+++ Database/Dump20260501.sql
@@ -0,0 +1,142 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `class` (
+  `c_id` int NOT NULL DEFAULT '1',
+  `c_date` datetime DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `paid_amount` float DEFAULT NULL,
+  `remaining_amount` float DEFAULT NULL,
+  `stu_id` int DEFAULT NULL,
+  PRIMARY KEY (`c_id`),
+  KEY `stu_c_id_idx` (`stu_id`),
+  CONSTRAINT `stu_c_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class`
+--
+
+LOCK TABLES `class` WRITE;
+/*!40000 ALTER TABLE `class` DISABLE KEYS */;
+/*!40000 ALTER TABLE `class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `revenue`
+--
+
+DROP TABLE IF EXISTS `revenue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `revenue` (
+  `r_id` int NOT NULL DEFAULT '1',
+  `date` datetime NOT NULL,
+  `amount` float DEFAULT NULL,
+  `tea_id` int DEFAULT NULL,
+  `stu_id` int DEFAULT NULL,
+  PRIMARY KEY (`r_id`),
+  KEY `tea_r_id_idx` (`tea_id`),
+  KEY `stu_r_id_idx` (`stu_id`),
+  CONSTRAINT `stu_r_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tea_r_id` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `revenue`
+--
+
+LOCK TABLES `revenue` WRITE;
+/*!40000 ALTER TABLE `revenue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `revenue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `stu_id` int NOT NULL DEFAULT '1',
+  `stu_name` varchar(120) DEFAULT NULL,
+  `phone` varchar(13) DEFAULT NULL,
+  `peyment_type` enum('monthly','twelivy','classy') DEFAULT 'monthly',
+  `tea_id` int DEFAULT NULL,
+  `p_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`stu_id`),
+  KEY `teacher_idx` (`tea_id`),
+  CONSTRAINT `teacher` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacher` (
+  `tea_id` int NOT NULL DEFAULT '1',
+  `tea_name` varchar(120) DEFAULT NULL,
+  `share` float DEFAULT NULL,
+  `no_classes` int DEFAULT NULL,
+  `unpaid_amount` int DEFAULT NULL,
+  `ispaid` tinyint DEFAULT '1',
+  PRIMARY KEY (`tea_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-01 13:26:14
